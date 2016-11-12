30588bd12ec73db6c4ca5b35de48420f075dbe3c
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-12 14:59:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,80 @@ var config={
     port:'5432',
   password:process.env.DB_PASSWORD  
 };
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-    user: 'harrishsreedhar',
-    database:'harrishsreedhar',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-  password:process.env.DB_PASSWORD 
-});
-var crypto=require('crypto');
 var app = express();
 app.use(morgan('combined'));
+/*var articles={
+   'article-one':{
+    title:"Article one",
+    heading:"article-one",
+    date:"sept 3243,34643",
+    content:` <p>
+            This is Article-one...Thi was created by me....is this even bearable??dgjktgdifgjnetkehrthervtnvrkjrkvtrkjvtr
+            rbvrgrknfbgrftbfrtjmrtfhfkhfbhjfh.bfh
+            fgbhlfgmhjbfjmflmhlfjlfufrlg
+            
+        </p>
+                <p>
+            This is Article-one...Thi was created by me....is this even bearable??dgjktgdifgjnetkehrthervtnvrkjrkvtrkjvtr
+            rbvrgrknfbgrftbfrtjmrtfhfkhfbhjfh.bfh
+            fgbhlfgmhjbfjmflmhlfjlfufrlg
+            
+        </p>
+       <p>
+            This is Article-one...Thi was created by me....is this even bearable??dgjktgdifgjnetkehrthervtnvrkjrkvtrkjvtr
+            rbvrgrknfbgrftbfrtjmrtfhfkhfbhjfh.bfh
+            fgbhlfgmhjbfjmflmhlfjlfufrlg
+            </p>`
+    
+},
+  'article-two':{
+       title:"Article 2222",
+    heading:"article-two",
+    date:"oct,3rd",
+    content:` <p>
+            This is Article-two...Thi was created by me....is this even bearable??dgjktgdifgjnetkehrthervtnvrkjrkvtrkjvtr
+            rbvrgrknfbgrftbfrtjmrtfhfkhfbhjfh.bfh
+            fgbhlfgmhjbfjmflmhlfjlfufrlg
+            
+        </p>
+                <p>
+            This is Article-two...Thi was created by me....is this even bearable??dgjktgdifgjnetkehrthervtnvrkjrkvtrkjvtr
+            rbvrgrknfbgrftbfrtjmrtfhfkhfbhjfh.bfh
+            fgbhlfgmhjbfjmflmhlfjlfufrlg
+            
+        </p>
+       <p>
+            This is Article-two...Thi was created by me....is this even bearable??dgjktgdifgjnetkehrthervtnvrkjrkvtrkjvtr
+            rbvrgrknfbgrftbfrtjmrtfhfkhfbhjfh.bfh
+            fgbhlfgmhjbfjmflmhlfjlfufrlg
+            </p>`
+    
+  },
+    'article-three':{
+         title:"Article three",
+    heading:"article-3",
+    date:"sept 34",
+    content:`
+    <p>
+            This is Article-3...Thi was created by me....is this even bearable??dgjktgdifgjnetkehrthervtnvrkjrkvtrkjvtr
+            rbvrgrknfbgrftbfrtjmrtfhfkhfbhjfh.bfh
+            fgbhlfgmhjbfjmflmhlfjlfufrlg
+            
+        </p>
+                <p>
+            This is Article-3...Thi was created by me....is this even bearable??dgjktgdifgjnetkehrthervtnvrkjrkvtrkjvtr
+            rbvrgrknfbgrftbfrtjmrtfhfkhfbhjfh.bfh
+            fgbhlfgmhjbfjmflmhlfjlfufrlg
+            
+        </p>
+       <p>
+            This is Article-3...Thi was created by me....is this even bearable??dgjktgdifgjnetkehrthervtnvrkjrkvtrkjvtr
+            rbvrgrknfbgrftbfrtjmrtfhfkhfbhjfh.bfh
+            fgbhlfgmhjbfjmflmhlfjlfufrlg
+            </p>`
+    
+    }
+};*/
 function createtemplate(data){
     var title=data.title;
     var heading=data.heading;
@@ -65,33 +128,6 @@ app.get('/test',function(req,res){
        }
    });   
 });
-function hash(input,salt){
-    var has=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return has.toString('hex');
-}
-app.get('/enter-data',function(req,res,next){
-    res.sendFile('ar1.html');
-});
-app.post('/create',function(req,res){
-    console.log('req.body');
-console.log(req.body);
-res.write('You sent the name "' + req.body.first+'".\n');
-res.end();
-    var sql = "INSERT INTO Test VALUES ?";
-var values =req.body.first;
-conn.query(sql, values, function(err) {
-    if (err) throw err;
-    conn.end();
-});
-});
- 
-
-app.get('/hash/:input',function(req,res){
-    var has=hash(req.params.input,'this-is-some-random-string');
-    res.send(has);  
-});
-
-
 var counter=0;
 app.get('/counter',function(req,res){
    counter+=1;
@@ -113,60 +149,6 @@ app.get('/', function (req, res) {
 //app.get('/article-one',function(req,res){
 //res.sendFile(path.join(__dirname,'article-one.html')); 
 //});
-app.get('/ar1',function(req,res){
-res.sendFile(path.join(__dirname,'ar1.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
-
-/*app.post('/create', function(req, res) {
-  //  var objBD = BD();
-var pool = new Pool(config);
-    var post = {
-        first: 1
-              };
-              pool.query ("INSERT INTO data ?", post, function(err, result) {
-                   if(err)
-         res.status(500).send(err.toString());
-                   
- 
-});
-console.log(query.sql);
-
-});*/
-/*var mysql = mysql.createConnection({
-host: HOST,
-port: PORT,
-user: MYSQL_USER,
-password: MYSQL_PASS,
-});
-app.get('/home',function(req,res,next){
-res.sendfile('views/forms.html');
-});
-app.post('/create', function(req, res) {
-console.log('req.body');
-console.log(req.body);
-res.write('You sent the name "' + req.body.first+'".\n');
-    
-res.end()
-
-mysql.query("Insert into "+TABLE+" (name,email,city,pincode) VALUES ('"+req.body.name+"','"+req.body.email+"','"+req.body.city+"','"+req.body.pincode+"')",function(err, result)      
-{                                                      
-  if (err)
-     throw err;
-});
-});
-
 app.get('/articles/:articleName',function(req,res){
 
 // pool.query("SELECT * FROM articles where title= '"+req.params.articleName+"'",function(err,result){
@@ -185,4 +167,16 @@ app.get('/articles/:articleName',function(req,res){
      }
  });
   
-});*/
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
