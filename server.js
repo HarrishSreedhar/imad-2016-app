2e3b2de71365e1fd2d41f6ad79897fa03969af05
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-17 20:37:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,8 @@ function createTemplate (data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'to1.html'));
+ app.get('/list', function (req, res) {
+  res.sendFile(path.join(__dirname,'t2.html')); 
 });
 //app.get('/to', function (req, res) {
   //res.sendFile(path.join(__dirname, 'to1.html'));
