7fd488465be2b13d666badc99a18a2d4a6fcb5e4
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-18 22:12:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,10 +75,7 @@ app.post('/clist', function (req, res) {
    // {"username": "tanmai", "password": "password"}
    // JSON
    var li = req.body.data;
-   //alert(li);
-   //var salt = crypto.randomBytes(128).toString('hex');
-  //var dbString = hash(password, salt);
-   pool.query('INSERT INTO new (data) VALUES ($1)', [li], function (err, result) {
+   pool.query('INSERT INTO list (id,data) VALUES ($1,$2)', [req.session.auth.userId,li], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
