4a23959233b4b018eac62ba526922f57800744fd
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-23 09:59:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -206,8 +206,9 @@ app.get('/view-list', function (req, res) {
         if (result.rows.length === 0) {
             res.status(404).send('Article not found');
         } else {
-            var articleData = result.rows[0];
-            res.send(JSON.stringify(result.rows));
+            for(i=0;i<result.length();i++){
+            var a = result.rows[i];
+            res.send(JSON.stringify(a));}
         }
     }
   });
