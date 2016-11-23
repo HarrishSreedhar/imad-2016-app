49a0450729ea530a33195570f7236be2481d2c9c
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-24 01:19:16 +0530

    [imad-console] Updates ui/tmain3.js

--- ui/tmain3.js
+++ ui/tmain3.js
@@ -19,11 +19,10 @@ function lo(){
                     var ul = document.getElementById('u');
                     for(var i = 0;i < o.length;i++){
                          var li = document.createElement("li");
-                           li.appendChild(document.createTextNode(i+1+""));
-    li.appendChild(document.createTextNode(")  "));
                          li.appendChild(document.createTextNode(o[i].data));
+var button = document.createElement("button");
 
-li.setAttribute("id",i+1);
+li.setAttribute("id",i);
 ul.appendChild(li);
                         
                      //    list += '<li>' +  + '</li>';
@@ -39,38 +38,3 @@ ul.appendChild(li);
         request.open('GET','http://harrishsreedhar.imad.hasura-app.io/view-list', true);
         request.send(null);
 }
-/*function del(){
-     alert("inside del");
-      var ul = document.getElementById('u');
-    for(var i=0;i<ul.length;i++)
-    {
-        var li=ul.getElementById(i);
-        var button = document.createElement("button");
-button.innerHTML = "delete";
- button.className = "b";
-li.append("  ");li.append("  ");
-li.append("  ");li.append("  ");
-li.append("  ");li.append("  ");
-li.append("  ");li.append("  ");
-li.append("  ");li.append("  ");
-li.append("  ");
-li.append("  ");
-li.append("  ");
-li.appendChild(button);
-li.setAttribute("id",i);
-ul.appendChild(li);
-        
-    }
-    /*var close = document.getElementsByClassName("b");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-      //var ul = document.getElementById("u");
-//var div = this.parentElement;
-  //  div.style.display = "none";
- var s=document.getElementById("0").innetText;
- 
-var m= s.innerText ||s.textContent;
- alert(m);*/
-}*/
- 
