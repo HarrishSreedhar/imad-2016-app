0136d6b78109605df753e5ca68b609642869725a
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-23 14:07:15 +0530

    [imad-console] Updates ui/tmain2.js

--- ui/tmain2.js
+++ ui/tmain2.js
@@ -121,4 +121,37 @@ list.addEventListener('click', function(ev) {
         <h3> Hi <i>${username}</i></h3>
         `;
 }
+function lo(){
+var submit = document.getElementById('vi');
+submit.onclick = function() {
+        
+        //Create a request object
+        var request = new XMLHttpRequest();
+    
+        //Capture the response and store it in a variable
+        request.onreadystatechange = function()
+        {
+            if(request.readyState === XMLHttpRequest.DONE)
+            {
+                //Take some action
+                if(request.status === 200)
+                {
+                    var names = request.responseText;
+                    names = JSON.parse(names);
+                    var list = '';
+                    for(var i = 0;i < names.length;i++){
+                         list += '<li>' + names[i] + '</li>';
+                    }
+                    var ul = document.getElementById('u');
+                    ul.innerHTML = list;
+                }
+            }
+        };
+    
+         //Render the variable in the correct span
+       
+        request.open('GET','http://harrishsreedhar.imad.hasura-app.io/view-list', true);
+        request.send(null);
+};
+}
 loadLogin();
