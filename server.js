ffa6c1a55daf18149d80b9c9958b18551f96feaa
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-23 13:29:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,15 +24,10 @@ app.get('/test',function(req,res){
        }
    });   
 });
-var counter=0;
-app.get('/counter',function(req,res){
-   counter+=1;
-   res.send(counter.toString());
-});
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-var names=[];
+
+
+
+
 app.get('/submit-name/',function(req,res){//URL:?name
     var name=req.query.name;
     names.push(name);
@@ -42,28 +37,64 @@ app.get('/submit-name/',function(req,res){//URL:?name
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'to1.html'));
 });
-//app.get('/article-one',function(req,res){
-//res.sendFile(path.join(__dirname,'article-one.html')); 
-//});
-app.get('/articles/:articleName',function(req,res){
-
-// pool.query("SELECT * FROM articles where title= '"+req.params.articleName+"'",function(err,result){
-  pool.query("SELECT * FROM articles where title=$1",[req.params.articleName],function(err,result){
-     if(err){
-         res.status(500).send(err.toString());
-     } else { 
-         if(result.rows.length===0)
-     {
-         res.status(404).send('Article not found');
-     } else
-     {
-         var articleData=result.rows[0];
-           res.send(createtemplate(articleData));
-     }
-     }
- });
-  
+ app.get('/list', function (req, res) {
+      na=req.query.uid;
+  res.sendFile(path.join(__dirname,'t2.html')); 
 });
+app.get('/ui/tmain.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'tmain.js'));
+});
+
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+   
+   na=req.body.username;
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO tuser (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send('Try another username');
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
+app.post('/clist', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var li = req.body.data;var uname;
+  /* pool.query('SELECT * FROM tuser WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+             uname=result.rows[0].username;    
+           }
+       });*/
+      pool.query('INSERT INTO list (data) where id=($1) VALUES ($2)', [req.session.auth.userId,li], function (err, result) {
+      // pool.query('INSERT INTO new (data) VALUES ($1)', [li], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.status(502).send('List successfully created:');
+      }
+   });
+});
+
+
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
