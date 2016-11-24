00ea46b9f587d993455cf3de816bbd3524a62c26
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-24 18:17:13 +0530

    [imad-console] Updates ui/tmain3.js

--- ui/tmain3.js
+++ ui/tmain3.js
@@ -1,11 +1,6 @@
 function lo(){
-
-       
-        //Create a request object
-        var request = new XMLHttpRequest();
-    
-        //Capture the response and store it in a variable
-        request.onreadystatechange = function()
+ var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
         {
             if(request.readyState === XMLHttpRequest.DONE)
             {
@@ -26,36 +21,21 @@ var button = document.createElement("button");
 
 li.setAttribute("id",i+1);
 ul.appendChild(li);
-//var t= s.substr(s.indexOf(")") + 1);
-                        
-                     //    list += '<li>' +  + '</li>';
+
                     }
                     
-                   // ul.innerHTML = list;
+                 
                 }
             }
         };
     
-         //Render the variable in the correct span
+        
        
         request.open('GET','http://harrishsreedhar.imad.hasura-app.io/view-list', true);
         request.send(null);
 }
 
-function ch() {
-    // Check if the user is already logged in
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-               // loadLoggedInUser(this.responseText);
-            } else {
-               // loadLoginForm();
-            }
-        }
-    };
-    
-    request.open('GET', '/check-login', true);
+GET', '/check-login', true);
     request.send(null);
 }
 
