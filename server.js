a2095a3ff2e280e23658244ec430d6307f1f41c4
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-24 15:09:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -61,33 +61,23 @@ app.post('/create-user', function (req, res) {
    var dbString = hash(password, salt);
    pool.query('INSERT INTO tuser (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
       if (err) {
-          res.status(500).send('Name already exixts ;( Try another username');
+          res.status(500).send('Try another username');
       } else {
-          res.send('User successfully created: ' + username+'Now click Login Button to unlock gr8 features');
+          res.send('User successfully created: ' + username);
       }
    });
-
+  /* pool.query('INSERT INTO list (id) VALUES ($1)', [req.session.auth.userId], function (err, result) {
+         if (err) {
+          res.status(500).send('name exists');
+      } else {
+          res.send('User id in DB: ');
+      }
+});*/
 });
-app.get('/check-login', function (req, res) {
-   if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM tuser WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else { na=result.rows[0].username;
-              res.send(result.rows[0].username);    
-           }
-       });
-   } else {
-       res.status(400).send('You are not logged in');
-   }
-});
-
+app.post('/clist', function (req, res) {
    // username, password
    // {"username": "tanmai", "password": "password"}
    // JSON
-//  if (req.session && req.session.auth && req.session.auth.userId) {
-app.post('/clist', function (req, res) {
    var li = req.body.data;var uname;
   /* pool.query('SELECT * FROM tuser WHERE id = $1', [req.session.auth.userId], function (err, result) {
            if (err) {
@@ -97,19 +87,14 @@ app.post('/clist', function (req, res) {
            }
        });*/
       pool.query('INSERT INTO list (id,data)  VALUES ($1,$2)', [req.session.auth.userId,li], function (err, result) {
-      
+      // pool.query('INSERT INTO new (data) VALUES ($1)', [li], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
           res.status(502).send('List successfully created:');
       }
    });
-
-   }
-  // else {
-    //   res.status(400).send('You are not logged in');
-   //}
-  
+});
 
 app.post('/dlist', function (req, res) {
    // username, password
@@ -159,7 +144,20 @@ app.post('/login', function (req, res) {
    });
 });
 
-
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM tuser WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else { na=result.rows[0].username;
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
@@ -188,21 +186,7 @@ app.post('/del-list', function (req, res) {
    // {"username": "tanmai", "password": "password"}
    // JSON
    var li = req.body.data;var uname;
-  /* pool.query('SELECT * FROM tuser WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-             uname=result.rows[0].username;    
-           }
-       });*/
-/*  pool.query('INSERT INTO list (id,data)  VALUES ($1,$2)', [req.session.auth.userId,li], function (err, result) {
-      // pool.query('INSERT INTO new (data) VALUES ($1)', [li], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.status(502).send('List successfully created:');
-      }
-   });*/
+  
 });
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
