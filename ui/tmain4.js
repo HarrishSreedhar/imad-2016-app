29da772c3f34c0279fa0471c64b5c7c329d9868d
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-24 09:18:31 +0530

    Create tmain4.js

---
+++ ui/tmain4.js
@@ -0,0 +1,43 @@
+function lo(){
+
+       
+        //Create a request object
+        var request = new XMLHttpRequest();
+    
+        //Capture the response and store it in a variable
+        request.onreadystatechange = function()
+        {
+            if(request.readyState === XMLHttpRequest.DONE)
+            {
+                //Take some action
+                if(request.status === 200)
+                {
+                    var names = request.responseText;
+                  var o=JSON.parse(names);
+                  
+
+                    var ul = document.getElementById('u');
+                    for(var i = 0;i < o.length;i++){
+                         var li = document.createElement("li");
+                          li.appendChild(document.createTextNode(i+1));
+                           li.appendChild(document.createTextNode(")"));
+                         li.appendChild(document.createTextNode(o[i].data));
+var button = document.createElement("button");
+
+li.setAttribute("id",i+1);
+ul.appendChild(li);
+//var t= s.substr(s.indexOf(")") + 1);
+                        
+                     //    list += '<li>' +  + '</li>';
+                    }
+                    
+                   // ul.innerHTML = list;
+                }
+            }
+        };
+    
+         //Render the variable in the correct span
+       
+        request.open('GET','http://harrishsreedhar.imad.hasura-app.io/view-list', true);
+        request.send(null);
+}
