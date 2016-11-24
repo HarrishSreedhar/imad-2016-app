18d2c03d3966aaa4a0e705aaab11b64fe3db6c09
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-24 14:54:53 +0530

    [imad-console] Updates ui/tmain2.js

--- ui/tmain2.js
+++ ui/tmain2.js
@@ -1,3 +1,4 @@
+var j;
 function addb()
 {
 	 var request = new XMLHttpRequest();
@@ -19,9 +20,48 @@ function addb()
      var data = document.getElementById('in').value;
       request.open('POST', '/clist', true);
         request.setRequestHeader('Content-Type', 'application/json');
-        // console.log(data);
+//alert(data+' is stored in database');// console.log(data);
        request.send(JSON.stringify({data:data}));  
      }
+
+
+		function del(k)
+		{  alert("inside del");
+		var table = document.getElementById("u"); 
+		    var it = document.getElementsByTagName("li");
+		  var s=it[k].innerText;
+		  alert(s+"deleted");
+			 /*var request = new XMLHttpRequest();
+				
+				// Capture the response and store it in a variable
+				request.onreadystatechange = function () {
+				  if (request.readyState === XMLHttpRequest.DONE) {
+					  // Take some action
+					  if (request.status === 200) {
+						  //alert('User created successfully');
+						  //register.value = 'Registered!';
+					  } else {
+						  //alert('Could not register the user');
+						  //register.value = 'Register';
+					  }
+				  }
+				};
+				
+			   
+				
+				request.open('GET', "/del-list?name="+i, true);
+			
+			}*/
+		
+		
+			
+		/*	 // Make the request---corresponding JS:
+			 'DELETE from list where user_id= $1 and id = $2',  [req.session.auth.userId],[req.query.name], function (err, result) {
+					if (err) {}
+		}*/
+}
+
+
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -34,12 +74,15 @@ var div = this.parentElement;
   };
  
 }
+/*  var ul = form.getElementsByTagName('u');
+ul = ul && ul[0];
+console.log(ul);*/
 
 
 
 
 
-
+// Create a new list item when clicking on the "Add" button
   function ele() {
      // static count = 1;
      //static $cou = 1;
@@ -62,6 +105,13 @@ var div = this.parentElement;
       document.getElementById("u").appendChild(li);
     }
     document.getElementById("in").value = "";
+    
+   /* var button = document.createElement("button");
+button.innerHTML = "Delete";
+li.appendChild(button);*/
+
+    
+
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
@@ -82,11 +132,33 @@ var div = this.parentElement;
   }
        
 
-
+/*function lo(){
+       
+        //Create a request object
+        var request = new XMLHttpRequest();
+    
+        //Capture the response and store it in a variable
+        request.onreadystatechange = function()
+        {
+            if(request.readyState === XMLHttpRequest.DONE)
+            {
+                //Take some action
+                if(request.status === 200)
+                {
+                    var names = request.responseText;
+                   names=JSON.getString.data;
+                    var list = '';
+                    for(var i = 0;i < names.length;i++){
+                        
+                         list += '<li>' + names[i] + '</li>';
+                    }
+                    var ul = document.getElementById('l');
+                    ul.innerHTML = list;
+                }
             }
         };
     
-         
+         //Render the variable in the correct span
        
         request.open('GET','http://harrishsreedhar.imad.hasura-app.io/view-list', true);
         request.send(null);
