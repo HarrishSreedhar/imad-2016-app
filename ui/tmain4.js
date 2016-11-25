94baaa814d4e31c1f79d9742a0209ce673a06fc2
HarrishSreedhar <harrishsreedharen97@gmail.com>
2016-11-25 09:14:08 +0530

    [imad-console] Updates ui/tmain4.js

--- ui/tmain4.js
+++ ui/tmain4.js
@@ -14,7 +14,9 @@ function lo(){
                 {
                     var names = request.responseText;
                   var o=JSON.parse(names);
-                  
+                  if(o===""){
+                      alert("No lists found!!...Add new lists");
+                  }
 
                     var ul = document.getElementById('u');
                     for(var i = 0;i < o.length;i++){
